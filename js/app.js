e0889d9b2f12424fa4356214e5e0109774be23d9
Zach Riggle <zach@riggle.me>
2013-06-09 13:03:55 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,99 @@
+// Ember.ENV.EXPERIMENTAL_CONTROL_HELPER = true;
+// Ember.LOG_BINDINGS = true;
+// Ember.ENV.RAISE_ON_DEPRECATION = true
+// Ember.LOG_STACKTRACE_ON_DEPRECATION = true
+
+window.App = Ember.Application.create({
+    rootElement: '#app',
+    // LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {
+  this.route("about", { path: "/about" });
+  this.route("favorites", { path: "/favs" });
+});
+
+App.Router.map(function() {
+  this.resource('taps', function() {
+      this.resource('tap', {path:':tap_id'});
+  })
+});
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('taps');
+  }
+})
+
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    this.controllerFor('nav').set('content', App.Tap.find());
+  },  
+  renderTemplate: function() {
+    this.render();
+    this.render("nav", {outlet: "nav", into: "application"})
+  }
+});
+
+App.TapsRoute = Ember.Route.extend({
+    model: function(params) {
+      console.log('hello')
+        return App.Tap.find(params.tap_id);
+    }
+});
+
+App.TapsController = Ember.ArrayController.extend({
+  sortProperties: ['id']
+});
+
+
+App.NavController = Ember.ArrayController.extend({
+});
+
+
+App.Store = DS.Store.extend({
+  revision: 12,
+  adapter: DS.FixtureAdapter.create()
+});
+
+
+App.Tap = DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  details: DS.attr('string'),
+  icon: DS.attr('string'),
+  style: DS.attr('string'),
+  kicked: DS.attr('number'),
+});
+// pink purple white orange
+App.Tap.FIXTURES = [{
+  id: 1,
+  name: 'Kicked!',
+  description: '',
+  details: 'This keg is empty.',
+  kicked: 1,
+  style: 'color: #FF69B4',
+},{
+  id: 2,
+  name: 'Hopdunk',
+  description: ' (10.8%)',
+  details: 'Aggressively hopped American Double IPA.  Features over a pound of hops and five different varieties.',
+  style: 'color: #71637D',
+},{
+  id: 3,
+  name: 'Cropshock',
+  description: ' (7%)',
+  details: 'Session Rye Pale Ale.  Three pounds of Rye malt made this a pain in the ass to mash, but it was worth it!',
+  style: 'color: #fff',
+},{
+  id: 4,
+  name: 'Kicked!',
+  description: '',
+  details: 'This keg is empty.',
+  kicked: 1,
+  style: 'color: #FF4500'
+}]
+
+Handlebars.registerHelper('icon', function(context, options){
+    return new Handletaps.SafeString("<i class='icon-" + context + "'></i>")
+})
