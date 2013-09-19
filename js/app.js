255a388cee53df665f98253de540df4af592f642
Zach <zachriggle@gmail.com>
2013-09-19 19:00:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -71,27 +71,28 @@ App.Tap.FIXTURES = [{
   description: ' (10%)',
   details: '(Homebrew) Made to the exact same specifications as the real deal as the world-reknowned stout by Founder\'s. Brewed with belgian bittersweet chocolate, roasted cacao nibs, and coffee.  Added Kentucky bourbon post-fermentation; aged one week on oak chips.',
   kicked: 1,
-  style: 'color: #FF69B4', 
+  style: 'color: #FF69B4; text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;', 
 },{
   id: 2,
   name: 'Dragon\'s Milk',
   description: ' (10%)',
   details: '(New Holland) A stout with roasty malt character intermingled with deep vanilla tones, all dancing in an oak bath',
-  style: 'color: #71637D',
+  style: 'color: #71637D; text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;',
 },{
   id: 3,
   name: 'Pumpkin',
   description: ' (5%)',
   details: '(Homebrew) Three pounds of pie pumpkin, and generous spices make this a yummy fall treat!',
   kicked: 1,
-  style: 'color: #fff', // white
+  style: 'color: #fff; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;', // white
 },{
   id: 4,
   name: 'Huma Lupa Licious',
   description: ' (7%)',
   details: '(Short\'s) AggressivelyNamed after the hop flower Humulus Lupulus, this India Pale Ale style beer has enormous amounts of hop bitterness, flavor and aroma.',
-  style: 'color: #FF4500', // orange
+  style: 'color: #FF4500; text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;', // orange
 }]
+  
 
 Handlebars.registerHelper('icon', function(context, options){
     return new Handletaps.SafeString("<i class='icon-" + context + "'></i>")
